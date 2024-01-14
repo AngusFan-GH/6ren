aca6f26133c6ff07e1b609346650d8d1144a025c
fanyingdong <winterfyd@gmail.com>
2024-01-14 23:38:59 +0800

    🐳 chore(webpack.config): 调整打包后的文件名

--- plugin/webpack.config.js
+++ plugin/webpack.config.js
@@ -1,6 +1,8 @@
 const { VueLoaderPlugin } = require('vue-loader');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require('path');
+const { name, version } = require('./package.json');
 
 module.exports = {
     mode: 'development',
@@ -9,7 +11,7 @@ module.exports = {
         background: './src/background.ts'
     },
     output: {
-        path: __dirname + '/dist',
+        path: resolve(__dirname, 'build/' + name + '-v' + version),
         filename: '[name].js'
     },
     resolve: {
@@ -57,9 +59,9 @@ module.exports = {
     plugins: [
         new VueLoaderPlugin(),
         new HtmlWebpackPlugin({
-            filename: 'popup.html', // 输出文件名
-            template: 'src/popup/popup.html', // 模板文件路径
-            chunks: ['popup'] // 指定包含的 chunk，与入口文件名称相对应
+            filename: 'popup.html',
+            template: 'src/popup/popup.html',
+            chunks: ['popup']
         }),
         new CopyPlugin({
             patterns: [
